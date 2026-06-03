e18224b82ba447e47f810af78da0276876238e20
EgorG23 <gusarow.egor2017@yandex.ru>
2026-06-04 02:35:34 +0300

    Update SQL-script for top 10 songs' history for last 7 days

--- sql_scripts/update_marts/top_10_songs_history_for_7_days.sql
+++ sql_scripts/update_marts/top_10_songs_history_for_7_days.sql
@@ -1,15 +1,23 @@
 CREATE OR REPLACE VIEW top_10_songs_history_for_7_days AS
+
 WITH latest_day AS (
     SELECT MAX(chart_date) AS max_date
     FROM spotify_daily_charts
 ),
 
 top10ofWeek AS (
-    SELECT
+    SELECT DISTINCT ON (track_name, artist_names)
         track_name,
-        artist_names
+        artist_names,
+        rank
     FROM spotify_daily_charts
     WHERE chart_date = (SELECT max_date FROM latest_day)
+    ORDER BY track_name, artist_names, rank
+),
+
+top10 AS (
+    SELECT *
+    FROM top10ofWeek
     ORDER BY rank
     LIMIT 10
 ),
@@ -18,10 +26,7 @@ deduplicated AS (
     SELECT DISTINCT ON (track_name, artist_names, chart_date)
         *
     FROM spotify_daily_charts
-    ORDER BY
-        track_name,
-        artist_names,
-        chart_date
+    ORDER BY track_name, artist_names, chart_date
 )
 
 SELECT
@@ -30,10 +35,8 @@ SELECT
     s.chart_date,
     s.rank,
     s.streams
-
 FROM deduplicated s
-JOIN top10ofWeek t
-    ON s.track_name = t.track_name
-    AND s.artist_names = t.artist_names
+JOIN top10 t ON s.track_name = t.track_name
+AND s.artist_names = t.artist_names
 WHERE s.chart_date >= (SELECT max_date FROM latest_day) - INTERVAL '6 days'
 ORDER BY s.track_name, s.artist_names, s.chart_date;
